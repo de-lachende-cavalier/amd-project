845c9018d60decc5832711e70113678282c97f8b
de-lachende-cavalier <mowzhja@protonmail.com>
2023-06-27 17:20:24 +0200

    adjusted the code to mantain formatting the same

--- split_reviews.py
+++ split_reviews.py
@@ -1,45 +1,46 @@
-import json
 """
 This file contains the code necessary to split the review file in three files, one containig the training data, one containig the validation data and one containing the testing data.
 
 The split is done to better operate on them when using online learning
 """
+import random
 
-def split_data_file(input_file, train_file, val_file, test_file, train_ratio=0.70, val_ratio=0.15, test_ratio=0.15):
-    total_lines = 6990280 # from https://www.yelp.com/dataset
+def split_file(input_file_path, train_output_path, val_output_path, test_output_path, train_ratio=0.7, val_ratio=0.15, seed=None):
+    if seed is not None:
+        random.seed(seed)
 
-    train_lines = int(train_ratio * total_lines)
-    val_lines = int(val_ratio * total_lines)
-    test_lines = int(test_ratio * total_lines)
+    with open(input_file_path, 'r') as input_file:
+        train_file = open(train_output_path, 'w')
+        val_file = open(val_output_path, 'w')
+        test_file = open(test_output_path, 'w')
 
-    with open(input_file, 'r') as input_f:
-        with open(train_file, 'w') as train_f:
-            for _ in range(train_lines):
-                line = json.loads(input_f.readline())
-                json.dump(line, train_f, indent=2)
-        print("[+] Done with training split.")
+        for line in input_file:
+            # determine which file to write to based on the random ratio
+            rand_value = random.random()
+            if rand_value < train_ratio:
+                output_file = train_file
+            elif rand_value < train_ratio + val_ratio:
+                output_file = val_file
+            else:
+                output_file = test_file
 
-        with open(val_file, 'w') as val_f:
-            for _ in range(val_lines):
-                line = json.loads(input_f.readline())
-                json.dump(line, val_f, indent=2)
-        print("[+] Done with validation split.")
+            output_file.write(line)
 
-        with open(test_file, 'w') as test_f:
-            for _ in range(test_lines):
-                line = json.loads(input_f.readline())
-                json.dump(line, test_f, indent=2)
-        print("[+] Done with test split.")
+        train_file.close()
+        val_file.close()
+        test_file.close()
 
-    print("[+] Splitting complete!")
+        print("[+] Splitting complete.")
 
 
-data_dir = "yelp-data/" 
-review_file = "yelp_academic_dataset_review.json"
-# Example usage
-split_data_file(
-    data_dir + review_file,
-    data_dir + "train_" + review_file, 
-    data_dir + "val_" + review_file, 
-    data_dir + "test_" + review_file, 
-    train_ratio=0.70, val_ratio=0.15, test_ratio=0.15)
+if __name__ == "__main__":
+    data_dir = "yelp-data/" 
+    review_file = "yelp_academic_dataset_review.json"
+
+    print("[+] Commencing splitting...")
+    split_file(
+        data_dir + review_file,
+        data_dir + "train_" + review_file, 
+        data_dir + "val_" + review_file, 
+        data_dir + "test_" + review_file, 
+        train_ratio=0.70, val_ratio=0.15)
